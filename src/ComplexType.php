707d11ebcc13600ce86a711d3149ba13139e2f74
Thomas Hansen <thomas@thomasphansen.dk>
2018-10-04 11:00:49 +0200

    .

--- src/ComplexType.php
+++ src/ComplexType.php
@@ -5,7 +5,9 @@
  */
 namespace Wsdl2PhpGenerator;
 
+use DateTime;
 use \Exception;
+use RuntimeException;
 use Wsdl2PhpGenerator\PhpSource\PhpClass;
 use Wsdl2PhpGenerator\PhpSource\PhpDocComment;
 use Wsdl2PhpGenerator\PhpSource\PhpDocElementFactory;
@@ -59,10 +61,10 @@ class ComplexType extends Type
      *
      * @throws Exception if the class is already generated(not null)
      */
-    protected function generateClass()
+    protected function generateClass(): void
     {
-        if ($this->class != null) {
-            throw new Exception("The class has already been generated");
+        if ($this->class !== null) {
+            throw new RuntimeException('The class has already been generated');
         }
 
         $classBaseType = $this->getBaseTypeClass();
@@ -113,7 +115,7 @@ class ComplexType extends Type
             $this->class->addVariable($var);
 
             if (!$member->getNullable()) {
-                if ($type == '\DateTime') {
+                if ($type === DateTime::class) {
                     if ($this->config->get('constructorParamsDefaultToNull')) {
                         $constructorSource .= "\t".'$this->' . $name . ' = $' . $name . ' ? $' . $name . '->format(\DateTime::ATOM) : null;' . PHP_EOL;
                     } else {
@@ -128,7 +130,7 @@ class ComplexType extends Type
 
             $getterComment = new PhpDocComment();
             $getterComment->setReturn(PhpDocElementFactory::getReturn($strCommentType, ''));
-            if ($type == '\DateTime') {
+            if ($type === DateTime::class) {
                 $getterCode =
 					"\t".'if ($this->' . $name . ' == null)' . PHP_EOL .
 					"\t".'{' . PHP_EOL .
@@ -154,7 +156,7 @@ class ComplexType extends Type
             $setterComment = new PhpDocComment();
             $setterComment->addParam(PhpDocElementFactory::getParam($strCommentType, $name, ''));
             $setterComment->setReturn(PhpDocElementFactory::getReturn($this->phpNamespacedIdentifier, ''));
-            if ($type == '\DateTime') {
+            if ($type === DateTime::class) {
                 if ($member->getNullable()) {
                     $setterCode =
 						"\t".'if ($' . $name . ' == null)' . PHP_EOL .
@@ -181,11 +183,11 @@ class ComplexType extends Type
                     // If the type of a member is nullable we should allow passing null to the setter. If the type
                     // of the member is a class and not a primitive this is only possible if setter parameter has
                     // a default null value. We can detect whether the type is a class by checking the type hint.
-                    $member->getNullable() && !empty($typeHint)
+                    $member->getNullable() && ( ($typeHint ?? '') !== '')
                 ),
                 $setterCode,
                 $setterComment,
-				'void'
+				$this->phpNamespacedIdentifier
             );
             $accessors[] = $setter;
         }
@@ -215,8 +217,8 @@ class ComplexType extends Type
      *   Returns a string containing the PHP identifier for the parent class
      *   or null if there is no applicable parent class.
      */
-    public function getBaseTypeClass()
-    {
+    public function getBaseTypeClass(): ?string
+	{
         // If we have a base type which is different than the current class then extend that.
         // It is actually possible to have different classes with the same name as PHP SoapClient has a poor
         // understanding of namespaces. Two types with the same name but in different namespaces will have the same
@@ -234,8 +236,8 @@ class ComplexType extends Type
      * @return ComplexType|null
      *   The base type or null if the type has no base type.
      */
-    public function getBaseType()
-    {
+    public function getBaseType(): ?ComplexType
+	{
         return $this->baseType;
     }
 
@@ -244,37 +246,37 @@ class ComplexType extends Type
      *
      * @param ComplexType $type
      */
-    public function setBaseType(ComplexType $type)
-    {
+    public function setBaseType(ComplexType $type): void
+	{
         $this->baseType = $type;
     }
 
     /**
      * @return bool
      */
-    public function getAbstract()
-    {
+    public function getAbstract(): bool
+	{
         return $this->abstract;
     }
 
     /**
      * @param bool $abstract
      */
-    public function setAbstract($abstract)
-    {
+    public function setAbstract($abstract): void
+	{
         $this->abstract = $abstract;
     }
 
 	/**
-	 * Adds the member. Owerwrites members with same name
+	 * Adds the member. Overwrites members with same name
 	 *
 	 * @param string $type
 	 * @param string $name
 	 * @param bool $nullable
 	 * @param string $strExtraType
 	 */
-    public function addMember($type, $name, $nullable, string $strExtraType = '')
-    {
+    public function addMember(string $type, string $name, bool $nullable, string $strExtraType = ''): void
+	{
         $this->members[$name] = new Variable($type, $name, $nullable, $strExtraType);
     }
 
@@ -283,8 +285,8 @@ class ComplexType extends Type
      *
      * @return Variable[]
      */
-    public function getMembers()
-    {
+    public function getMembers(): array
+	{
         return $this->members;
     }
 
@@ -297,8 +299,8 @@ class ComplexType extends Type
      * @param bool $defaultNull Whether to set the default value of parameters to null.
      * @return string The parameter string.
      */
-    protected function buildParametersString(array $parameters, $includeType = true, $defaultNull = false)
-    {
+    protected function buildParametersString(array $parameters, bool $includeType = true, bool $defaultNull = false): string
+	{
         $parameterStrings = [];
         foreach ($parameters as $name => $type) {
             $parameterString = '$' . $name;
@@ -320,9 +322,9 @@ class ComplexType extends Type
      * @param ComplexType $type The type to retrieve base type members from.
      * @return Variable[] Member variables from all base types.
      */
-    protected function getBaseTypeMembers(ComplexType $type)
-    {
-        if (empty($type->baseType)) {
+    protected function getBaseTypeMembers(ComplexType $type): array
+	{
+        if ($type->baseType === null) {
             return [];
         }
 
